0bd599a06d6a7727cad0f7f597abb54b8918a24b
bgogetap <bgogetap@gmail.com>
2015-10-11 21:06:10 -0500

    Started animation

--- app/src/main/java/com/cultureoftech/draggablelinearlayout/MyDragListener.java
+++ app/src/main/java/com/cultureoftech/draggablelinearlayout/MyDragListener.java
@@ -2,6 +2,7 @@ package com.cultureoftech.draggablelinearlayout;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
  */
 public class MyDragListener implements View.OnDragListener {
 
+    public static int initialItemHeight = 0;
+    public static View placeholderView;
+
     private final Context context;
     private Drawable enterShape, normalShape;
 
@@ -29,6 +33,9 @@ public class MyDragListener implements View.OnDragListener {
     public boolean onDrag(View v, DragEvent event) {
         int action = event.getAction();
         final View draggingView = (View) event.getLocalState();
+        if (initialItemHeight == 0) {
+            initialItemHeight = draggingView.getLayoutParams().height;
+        }
         ViewGroup originalParent = (ViewGroup) draggingView.getParent();
         ViewGroup eventParent = (ViewGroup) v;
         switch (action) {
@@ -38,7 +45,6 @@ public class MyDragListener implements View.OnDragListener {
             case DragEvent.ACTION_DRAG_ENTERED:
                 if (v == originalParent) {
                     draggingView.getLayoutParams().height = WRAP_CONTENT;
-                    draggingView.requestLayout();
                 }
                 v.setBackgroundDrawable(enterShape);
                 break;
@@ -46,10 +52,41 @@ public class MyDragListener implements View.OnDragListener {
                 // TODO Check for child views vs. dragged item position and animate
                 int x = (int)event.getX();
                 int y = (int)event.getY();
+                if (placeholderView == null) {
+                    placeholderView = new View(eventParent.getContext());
+                }
                 if (eventParent.getChildCount() == 0) {
-                    View view = new View(eventParent.getContext());
-                    eventParent.addView(view);
-                    view.getLayoutParams().height = 100;
+                    if (placeholderView.getParent() != null) {
+                        ((ViewGroup)placeholderView.getParent()).removeView(placeholderView);
+                    }
+                    eventParent.addView(placeholderView);
+                    instantCollapseThenExpand(placeholderView);
+                } else {
+                    Log.d("View child count", "" + eventParent.getChildCount());
+                    for (int i = 0; i < eventParent.getChildCount(); i++) {
+                        if (placeholderView.getParent() != null) {
+                            ((ViewGroup)placeholderView.getParent()).removeView(placeholderView);
+                        }
+                        if (!isPointInsideView(x, y, eventParent.getChildAt(i))) {
+                            if (isAtUpperHalf(x, y, eventParent.getChildAt(i))) {
+                                if (i > 0) {
+                                    eventParent.addView(placeholderView, i - 1);
+                                    instantCollapseThenExpand(placeholderView);
+                                } else {
+                                    eventParent.addView(placeholderView, 0);
+                                    instantCollapseThenExpand(placeholderView);
+                                }
+                            } else {
+                                if (i == eventParent.getChildCount() - 1) {
+                                    eventParent.addView(placeholderView);
+                                    instantCollapseThenExpand(placeholderView);
+                                } else {
+                                    eventParent.addView(placeholderView, i + 1);
+                                    instantCollapseThenExpand(placeholderView);
+                                }
+                            }
+                        }
+                    }
                 }
                 break;
             case DragEvent.ACTION_DRAG_EXITED:
@@ -109,6 +146,55 @@ public class MyDragListener implements View.OnDragListener {
     }
 
     private static void expand(final View v) {
+        Animation expandAnim = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime < 1) {
+                    v.getLayoutParams().height = (int) (initialItemHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
 
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        expandAnim.setDuration(400);
+        v.startAnimation(expandAnim);
+    }
+
+    private static void instantCollapseThenExpand(final View v) {
+        v.getLayoutParams().height = 0;
+        v.requestLayout();
+        expand(v);
+    }
+
+    public static boolean isPointInsideView(float x, float y, View view){
+        int location[] = new int[2];
+        view.getLocationOnScreen(location);
+        int viewX = location[0];
+        int viewY = location[1];
+
+        //point is inside view bounds
+        if(( x > viewX && x < (viewX + view.getWidth())) &&
+                ( y > viewY && y < (viewY + view.getHeight()))){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isAtUpperHalf(float x, float y, View view) {
+        int location[] = new int[2];
+        view.getLocationOnScreen(location);
+        int viewX = location[0];
+        int viewY = location[1];
+
+        if (y > (viewY + (0.5 * view.getHeight()))) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
